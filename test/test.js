690dbd9b3508ecf7e4042ae5f87bfc816fc71917
liuxingyu521 <xuj_power@qq.com>
2022-10-14 23:42:24 +0800

    feat: add json file lint rule

---
+++ test/test.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+const a = 123
+
+// eslint-disable-next-line no-unused-vars
+const arr = [1, 2, 3]
+
+// eslint-disable-next-line no-unused-vars
+const arr2 = [
+  1,
+  2,
+  3,
+]
